a68ad8d775a2a5e51669408e92b1e35993737aab
Aidai205 <122851720+Aidai205@users.noreply.github.com>
2023-04-11 14:57:28 +0600

    Add files via upload

---
+++ frontend osnovy/urok23/script.js
@@ -0,0 +1,16 @@
+var friendsName = "Aidai"
+let age = 12
+const favoriteBook = "Harry Potter"
+
+friendsName = "Tima"
+age = 13
+
+
+console. log (friendsName, age, favoriteBook)
+
+const summ= 5 ** 155
+
+console.log (summ)
+
+
+
